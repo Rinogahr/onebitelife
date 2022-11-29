8774e55e77e6577c101cb1aa19d59461e833b00b
Rodrigo Maciel <69148290+Rinogahr@users.noreply.github.com>
2022-11-28 23:52:12 -0300

    stilização da pagina start

--- src/Pages/Start/Start.jsx
+++ src/Pages/Start/Start.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { View, ScrollView, Image, Text } from "react-native";
+import { View, ScrollView, Image, Text, StyleSheet } from "react-native";
 export default function Start(){
     return(
-        <View>
+        <View style={myStyle.container}>
             <ScrollView showsVerticalScrollIndicator={false}>
-                <View>
-                    <Image source={require('../../assets/icons/logo3.png')}/>
-                    <Text>
-                        Vamos transformar sua vida {"/n"} em jogo, buscando sempre {"/n"} o
+                <View style={{ alignItems: "center"}}>
+                    <Image style={myStyle.logo} source={require('../../assets/icons/logo3.png')}/>
+                    <Text style={myStyle.desciption}>
+                        Vamos transformar sua vida {"\n"} em jogo, buscando sempre {"\n"} o
                         melhor nivel.
                     </Text>
                 </View>
@@ -15,3 +15,22 @@ export default function Start(){
         </View>
     );
 }
+
+const myStyle = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: "#af0bf0"
+    },
+     logo:{
+        width: 300,
+        height: 60,
+        marginTop: 60,
+        marginBottom: 20
+     },
+     desciption:{
+        color: "#fafafa",
+        fontSize: 20,
+        textAlign: "center",
+        marginVertical: 60,
+     }
+});
