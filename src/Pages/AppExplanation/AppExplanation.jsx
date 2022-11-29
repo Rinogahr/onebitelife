da43bdb331b66fafa76f27975f8f2fb08e97099f
Rodrigo Maciel <69148290+Rinogahr@users.noreply.github.com>
2022-11-29 01:03:20 -0300

    criando o componente AppEplanation.jsx vou dormir pois já é 1h da manhã

---
+++ src/Pages/AppExplanation/AppExplanation.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { View, ScrollView, Image, Text, StyleSheet } from "react-native";
+import DefaultButton from "../../Components/Common/DefaultButton/DefaultButton";
+
+export default function AppExplanation(){
+    const handleShoWHome = () =>{
+        console.log("handleShoWHome");
+    }
+    return(
+        <View style={myStyle.container}>
+            <ScrollView>
+                <View style={{alignItems: "center"}}>
+                    <Text style={myStyle.title}>Antes, deixa {"\n"} eu te explicar...</Text>
+                    <Text style={myStyle.descriptionCta}>
+                        Pronto(a) para subir de nível na vida?
+                    </Text>
+                    <Text style={myStyle.description}>
+                        Na proxima tela você vai  poder escolher {"\n"}
+                        seus 4 hábitos de forma individual.
+                    </Text>
+                    <DefaultButton
+                        buttonText={"Continuar"}
+                        handlePress={handleShoWHome}
+                        width={250}
+                        height={50}
+                    />
+                </View>
+            </ScrollView>
+        </View>
+    );
+}
+
+const myStyle = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: "#af0bf0",
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "#fafafa",
+        textAlign: "center",
+        marginVertical: 40
+    },
+    descriptionCta:{
+        color: "#fafafa",
+        fontWeight: "bold",
+        fontSize: 16,
+        marginTop: 20,
+        marginBottom: 10,
+    },
+    description:{
+        color: "#fafafafa",
+        textAlign: "center",
+        marginBottom: 30,
+    }
+});
