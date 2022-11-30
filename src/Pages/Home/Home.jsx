f615bf6f39f4258aa41f092864729b5fce151857
Rodrigo Maciel <69148290+Rinogahr@users.noreply.github.com>
2022-11-29 22:38:27 -0300

    estrutura pagina home e navegação

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { View, ScrollView, Image, Text, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import LifeStatus from "../../Components/Common/LifeStatus/LifeStatus";
 
 
 export default function Home(){
     const navegacao = useNavigation();
-
+    function handleNavExplanation(){
+        navegacao.navigate("AppExplanation");
+    }
 
     return(
         <View style={myStyle.container}>
-            <Text>TELA DE HOME VAMOS CRIAR</Text>
-
+            <ScrollView>
+                <View style={{alignItems: "center"}}>
+                    <Text style={myStyle.dailyCheks}>❤️ 20 dias - ✔️ 80 checks</Text>
+                    <LifeStatus/>
+                </View>
+                <Text style={myStyle.explanationText}
+                onPress={ () => handleNavExplanation()}>Ver explicação novamente</Text>
+            </ScrollView>
         </View>
     );
 }
@@ -18,6 +27,21 @@ export default function Home(){
 
 const myStyle = StyleSheet.create({
     container:{
-
+        flex: 1,
+        backgroundColor: "#292A4C",
+    },
+    dailyCheks:{
+        color: "#fafafa",
+        fontWeight: "bold",
+        textAlign: "center",
+        fontSize: 16,
+    },
+    explanationText:{
+        color: "#fafafa",
+        fontWeight: "bold",
+        textAlign: "center",
+        fontSize: 16,
+        paddingTop: 15,
+        paddingBottom: 25,
     }
 });
